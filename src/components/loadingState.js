379a4b6bb18cd777da13b8ed110843cad0617d9b
Henri <henri.hoyez@gmail.com>
2020-12-21 15:54:30 +0100

    Add the loading spinner.

--- src/components/loadingState.js
+++ src/components/loadingState.js
@@ -1,13 +1,14 @@
 import React from 'react';
-// import { Spinner } from 'react-bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import { CircularProgress } from '@material-ui/core';
 
 
 export default function LoadingState() {
     return (
 
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop:'100px' }}>
             <strong style={{marginRight: '10px', fontSize:'xx-large'}}>Computing best scenario...</strong>
+            <CircularProgress color='inherit' thickness={5} size={30}/>
+
         </div>
     )
 
